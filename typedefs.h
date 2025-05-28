f3f2343c65a607ca632ed9e0fa90fce886b5ab33
Ryan J. Blake <ryan.blake@gmail.com>
2025-05-27 23:50:29 -0400

    Init after finishing part 1 of tutorial

---
+++ typedefs.h
@@ -0,0 +1,17 @@
+#ifndef TYPEDEFS_H
+#define TYPEDEFS_H
+
+#include <math.h>
+#include <stdbool.h>
+
+#ifndef M_PI
+#define M_PI 3.1415926535897932384626433
+#endif
+
+typedef struct _vec2
+{
+    double x;
+    double y;
+} vec2_t;
+
+#endif
